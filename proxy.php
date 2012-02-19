7ea8bdc38208e2377d3a98412aa97bd6d6a8729e
Jason Weaver <indyplanets@gmail.com>
2012-02-19 13:36:25 -0600

    first commit

---
+++ proxy.php
@@ -0,0 +1,34 @@
+<?php
+$path = ($_POST['path']) ? $_POST['path'] : $_GET['path'];
+$url = $path;
+ 
+// Open the Curl session
+$session = curl_init($url);
+ 
+// If it's a POST, put the POST data in the body
+if ($_POST['path']) {
+	$postvars = '';
+	while ($element = current($_POST)) {
+		$postvars .= urlencode(key($_POST)).'='.urlencode($element).'&';
+		next($_POST);
+	}
+	curl_setopt ($session, CURLOPT_POST, true);
+	curl_setopt ($session, CURLOPT_POSTFIELDS, $postvars);
+}
+ 
+// Don't return HTTP headers. Do return the contents of the call
+curl_setopt($session, CURLOPT_HEADER, false);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+ 
+// Make the call
+$xml = curl_exec($session);
+ 
+// does the service return XML or JSON? Set the Content-Type appropriately
+$headerType = ($_POST['type']) ? $_POST['type'] : $_GET['type'];
+// 'text/xml' or 'application/json'
+$headerType = ($headerType) ? $headerType : 'text/xml';//default
+header("Content-Type: " . $headerType);
+ 
+echo $xml;
+curl_close($session);
+?>
